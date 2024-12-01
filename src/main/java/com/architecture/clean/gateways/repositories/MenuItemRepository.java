b03d60cf88a01687cfb3bbbd57052909d52b684a
gpossas <possasg@gmail.com>
2024-12-01 09:26:35 -0300

    add: MenuItemRepository

---
+++ src/main/java/com/architecture/clean/gateways/repositories/MenuItemRepository.java
@@ -0,0 +1,19 @@
+package com.architecture.clean.gateways.repositories;
+
+import com.architecture.clean.core.domain.MenuItem;
+import com.architecture.clean.infra.persistence.entities.MenuItemEntity;
+import com.architecture.clean.web.dtos.menu_item.ResponseMenuItemDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public interface MenuItemRepository
+{
+    MenuItemEntity saveMenuItem(MenuItem menuItem);
+    Optional<MenuItemEntity> getMenuItemById(UUID id);
+    List<MenuItemEntity> getAllMenuItems();
+    void deleteMenuItemById(UUID id);
+}
